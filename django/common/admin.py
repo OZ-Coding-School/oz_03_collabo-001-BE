516dd5cabbd89dc70d0e69c303977159a4c62be0
bamjun <khy5116@naver.com>
2024-08-13 11:05:20 +0900

    Feat:register banneradmin model on admin site (#13)

--- django/common/admin.py
+++ django/common/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Banner
+
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ("id", "category", "url_link")  # 목록 화면에 표시할 필드들
+    search_fields = ("category",)  # 검색할 수 있는 필드
+    ordering = ("id",)  # 목록을 정렬할 기준 필드
+
+
+# 모델을 Admin 사이트에 등록
+admin.site.register(Banner, BannerAdmin)
