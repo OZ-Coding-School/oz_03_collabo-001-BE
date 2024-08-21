798ce7e3ec983499076eb861ffbd9e59b0d59acf
bamjun <khy5116@naver.com>
2024-08-22 00:07:38 +0900

    Feat:avg rating (#68)

--- django/places/apps.py
+++ django/places/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PlacesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "places"
+
+    def ready(self):
+        import places.signals
+
---
+++ django/places/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import Avg
+import math
+
+from.models import Comments
+
+@receiver(post_save, sender=Comments)
+def update_place_rating(sender, instance, created, **kwargs):
+    place = instance.place
+    
+    # 댓글의 평균 rating 계산
+    avg_rating = place.comments.aggregate(Avg('rating'))['rating__avg']
+
+    if avg_rating is not None:
+        # 소수점 3째자리에서 반올림하여 rating_float에 저장
+        place.rating_float = round(avg_rating, 3)
+        # 소수점 1째자리에서 반올림하여 rating에 저장
+        place.rating = round(avg_rating)
+
+        place.save()
