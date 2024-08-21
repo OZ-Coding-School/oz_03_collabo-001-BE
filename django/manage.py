b88a35713dce29e567b9853d7650a9f3b072c6b4
youngkwangjoo <dudrknd1642@gmail.com>
2024-08-21 16:53:39 +0900

    manage.py 부분 os에 따라 setting 선택하는 코드

--- django/manage.py
+++ django/manage.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import platform
 import sys
 
 
 def main():
     """Run administrative tasks."""
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.product")
+    # 운영 체제 확인
+    current_os = platform.system()
+
+    if current_os == "Darwin":  # macOS
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development_sqlite")
+    elif current_os == "Linux":
+        # Amazon Linux를 더 정확하게 확인하려면 추가 검사가 필요할 수 있습니다.
+        # 여기서는 Linux를 Amazon Linux로 가정합니다.
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.product")
+    else:
+        # 기본값 설정 (다른 OS의 경우)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.product")
 
     try:
         from django.core.management import execute_from_command_line
