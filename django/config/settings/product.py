af1c8067855131fc04257c1614c30e57d1805856
youngkwangjoo <dudrknd1642@gmail.com>
2024-08-26 13:45:36 +0900

    cors origin false 로 임시 수정

--- django/config/settings/product.py
+++ django/config/settings/product.py
@@ -8,9 +8,11 @@ ALLOWED_HOSTS = ["api.dogandbaby.co.kr", "www.dogandbaby.co.kr", "13.125.130.26"
 CORS_ALLOWED_ORIGINS = [
     "http://api.dogandbaby.co.kr",
     "https://api.dogandbaby.co.kr",
+    "http://www.dogandbaby.co.kr",
+    "https://www.dogandbaby.co.kr",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://api.dogandbaby.co.kr"]
+CSRF_TRUSTED_ORIGINS = ['https://api.dogandbaby.co.kr']
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True  # 쿠키 및 인증 헤더를 허용합니다.
@@ -19,7 +21,7 @@ SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True  # HTTPS에서만 CSRF 쿠키 전송
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 DATABASES = {
